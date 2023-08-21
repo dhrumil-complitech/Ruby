c7cd98e82b46f99212ab01e3d48ece46745a505e
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-21 13:48:28 +0530

    Ruby Program

---
+++ Program_21.rb
@@ -0,0 +1,12 @@
+print "Enter first number: ";
+a = gets.chomp.to_i;  
+
+print "Enter second number: ";
+b = gets.chomp.to_i;  
+
+print "Enter third number: ";
+c = gets.chomp.to_i;  
+
+largest=a>b ? a>c ?a : c : b>c ? b : c;        
+        
+print "Largest number is : ", largest;
