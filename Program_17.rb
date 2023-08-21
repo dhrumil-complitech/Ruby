6f7fa48d35034aa7b4b81136983604f47fd99780
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-21 12:16:49 +0530

    Ruby Program

--- Program_17.rb
+++ Program_17.rb
@@ -1,5 +1,11 @@
-print "Enter number: ";
-num = gets.chomp.to_i;  
-
-msg=num>0 ? "POSITIVE" : "NEGATIVE"; 
-print "Number is: ",msg;
+def check_positive_negative(number)
+    result = number > 0 ? "POSITIVE" : (number < 0 ? "NEGATIVE" : "0 Is Neither Positive Nor Negative")
+    result
+  end
+  
+  print "Enter a number: "
+  num = gets.chomp.to_i
+  
+  result = check_positive_negative(num)
+  puts "The Number is #{result}"
+  
