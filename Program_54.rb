b6f1e0cf993b6b0db2d934468bcb2b814efa47bc
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-22 15:55:41 +0530

    Ruby Program

--- Program_54.rb
+++ Program_54.rb
@@ -1,10 +1,10 @@
 
 print "Enter the marked price: "
-marked_price = gets.chomp.to_f
+mrp_price = gets.chomp.to_f
 
 print "Enter the selling price: "
 selling_price = gets.chomp.to_f
 
-discount_percentage = ((marked_price - selling_price).to_f / marked_price) * 100
+discount_percentage = ((mrp_price - selling_price).to_f / mrp_price) * 100
 
 puts "Discount Percentage: #{discount_percentage}%"
