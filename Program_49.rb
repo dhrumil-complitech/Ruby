b3caaf2ffb98116502a0940379ce401d82f8d89e
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-22 12:55:43 +0530

    Ruby Program

---
+++ Program_49.rb
@@ -0,0 +1,9 @@
+persons= {"1" =>"Dhrumil","2" => "Dhruvin","3" => "Ravi","4"=>"Rahul"}
+print "Enter a Student Name: "
+user_input = gets.chomp
+matching_keys = persons.select { |key, value| value == user_input }.keys
+if matching_keys.any?
+  puts "Person ID(s) for '#{user_input}': #{matching_keys.join(', ')}"
+else
+  puts "Person Name not found."
+end
