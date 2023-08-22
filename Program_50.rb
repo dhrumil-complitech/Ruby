848bb332fc5cf5cca5da5e22932005ce6cdf14b4
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-22 13:06:38 +0530

    Ruby Program

--- Program_50.rb
+++ Program_50.rb
@@ -2,9 +2,9 @@ def invert_hash(hash)
     inverted_hash = hash.invert
   end
   students = {"1" => "Dhrumil", "2" => "Ravi", "103" => "Rahul"}
-  inverted_students = invert_hash(students)
+  inverted_persons = invert_hash(persons)
   puts "Original Hash:"
-  puts students
+  puts persons
   puts "\nInverted Hash:"
-  puts inverted_students
+  puts inverted_persons
   
