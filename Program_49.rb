ae4b491e93b6b9be023868df3e331a4f13b6f1d3
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-22 13:14:08 +0530

    Ruby Program

--- Program_49.rb
+++ Program_49.rb
@@ -1,9 +1,15 @@
-persons= {"1" =>"Dhrumil","2" => "Dhruvin","3" => "Ravi","4"=>"Rahul"}
+persons = {"1" => "Dhrumil","2" => "Dhruvin","3" => "Ravi"}
 print "Enter a Person Name: "
 user_input = gets.chomp
-matching_keys = persons.select { |key, value| value == user_input }.keys
-if matching_keys.any?
-  puts "Person ID(s) for '#{user_input}': #{matching_keys.join(', ')}"
+matching_key = nil
+persons.each do |key, value|
+  if value == user_input
+    matching_key = key
+    break
+  end
+end
+if matching_key
+  puts "Person  ID for #{user_input}: #{matching_key}"
 else
   puts "Person Name not found."
 end
