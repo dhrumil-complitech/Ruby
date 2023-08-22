8d9837b9704fc90ef6c5d0a9673258d35c1a0d29
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-22 11:50:38 +0530

    Ruby Program

--- Program_43.rb
+++ Program_43.rb
@@ -5,4 +5,4 @@ curretDateTime = date_time.inspect();
 
 time_zone= date_time.zone();
 
-print "Current time  is: ",time_zone;
+print "Current time zone is: ",time_zone;
---
+++ Program_44.rb
@@ -0,0 +1,4 @@
+date_time = Time.new();
+curretDateTime = date_time.inspect();
+dayOfYear  = date_time.yday();
+print "Current day of year is: ",dayOfYear;
