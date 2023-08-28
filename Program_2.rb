2ad838d2862f7976cb4230ea0b50c5d6fd60ff6a
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-28 15:36:14 +0530

    Ruby Program

--- Program_2.rb
+++ Program_2.rb
@@ -1,9 +1,18 @@
-puts "Enter length:"
-l=gets.chomp.to_f
-puts "Enter width:"
-w=gets.chomp.to_f
- 
-area=l*w
-
-
-puts "Area of Rectangle is #{area}"
+begin
+    print "Enter the length of rectangle: "
+    length = gets.chomp
+    length = Integer(length)
+    rescue ArgumentError
+    puts "You have entered wrong length."
+    retry
+  end
+  begin
+    print "Enter the width of rectangle: "
+    width = gets.chomp
+    width = Integer(width)
+    rescue ArgumentError
+    puts "You have entered wrong width."
+    retry
+  end
+  area = length * width;
+  puts "Area of the rectangle is #{area}"
