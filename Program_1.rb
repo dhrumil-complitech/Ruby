5dcc984d2e59d37b0b029042c2e11a15bbaeac00
Dhrumil Sharma <dhrumil@complitech.net>
2023-08-28 15:04:02 +0530

    Ruby Program

--- Program_1.rb
+++ Program_1.rb
@@ -1,15 +1,18 @@
 begin
-    puts "Enter first value: "
-    num1_input = gets.chomp
-    num1 = Integer(num1_input)  
-    puts "Enter second value: "
-    num2_input = gets.chomp
-    num2 = Integer(num2_input) 
-    sum = num1 + num2
-    puts "The sum is #{sum}"
-  rescue ArgumentError
-    puts "Invalid input. Please enter valid number."
-  rescue => e
-    puts "An error occurred: #{e.message}"
+    print "Enter the first integer: "
+    num1 = gets.chomp
+    num1 = Integer(num1)
+    rescue ArgumentError
+    puts "You have entered wrong integer number."
+    retry
   end
-  
+  begin
+    print "Enter the second integer: "
+    num2 = gets.chomp
+    num2 = Integer(num2)
+    rescue ArgumentError
+    puts "You have entered wrong integer number."
+    retry
+  end
+    sum = num1 + num2
+    puts "The sum of #{num1} and #{num2} is #{sum}."
