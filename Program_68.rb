4b45ca3c1cd01ef36841f490b563b37568700938
Dhrumil Sharma <dhrumil@complitech.net>
2023-09-04 14:42:56 +0530

    Ruby Program

--- Program_68.rb
+++ Program_68.rb
@@ -1,15 +1,9 @@
-# Specify the file name you want to read
+
 file_name = "hello.txt"
-
 begin
-  # Open the file for reading
   file_content = File.read(file_name)
-
-  # Display the contents of the file
   puts "Contents of '#{file_name}':"
   puts file_content
-
 rescue => e
-  # Handle any exceptions that might occur
   puts "An error occurred: #{e.message}"
 end
