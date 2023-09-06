5568d04ce6d79067a4c453ddd822bd0b66f3431f
Dhrumil Sharma <dhrumil@complitech.net>
2023-09-06 11:15:50 +0530

    Ruby Program

--- Program_72.rb
+++ Program_72.rb
@@ -13,7 +13,7 @@ end
 
 current_date = Date.today
 if date_of_birth > current_date
-  puts "Please Enter Valid Bitrhdate"
+  puts "Please Enter Valid Birthdate"
 else
   age = current_date.year - date_of_birth.year
   age -= 1 if current_date.yday < date_of_birth.yday
