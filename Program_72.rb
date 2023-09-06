05c0bf11217bd86082dbe5abefe28ba245398ebe
Dhrumil Sharma <dhrumil@complitech.net>
2023-09-06 11:13:43 +0530

    Ruby Program

--- Program_72.rb
+++ Program_72.rb
@@ -1,12 +1,22 @@
 require 'date'
+
 print "Enter your date of birth (YYYY-MM-DD): "
-date_of_birth = gets.chomp
+date_of_birth_str = gets.chomp
+
 begin
-  dob = Date.parse(date_of_birth)
-  current_date = Date.today
-  age = current_date.year - dob.year
-  age -= 1 if current_date.month < dob.month || (current_date.month == dob.month && current_date.day < dob.day)
-  puts "You are #{age} years old."
+  date_of_birth = Date.parse(date_of_birth_str)
+
 rescue ArgumentError
-  puts "Invalid date of birth format. Please use YYYY-MM-DD."
+  puts "Invalid date format. Please use YYYY-MM-DD."
+  exit
+end
+
+current_date = Date.today
+if date_of_birth > current_date
+  puts "Please Enter Valid Bitrhdate"
+else
+  age = current_date.year - date_of_birth.year
+  age -= 1 if current_date.yday < date_of_birth.yday
+
+  puts "Your age is #{age} years."
 end
